6cfde541f9dce67627bfca6a6ad845ca0d08823b
Tharun reddy Devalapalli <tharun256652@gmail.com>
2025-12-12 10:47:28 +0530

    added deployment workflows

--- source_code/server.py
+++ source_code/server.py
@@ -1,94 +1,80 @@
 '''
 Created on Jan 10, 2017
-
-@author: hanif
+Author: hanif (updated for modern DevOps deployment)
 '''
 
+import os
 from flask import Flask, flash, render_template, redirect, url_for, request, session
 from module.database import Database
 
-
 app = Flask(__name__)
-app.secret_key = "mys3cr3tk3y"
+
+# Secure Secret Key for session handling
+app.secret_key = os.environ.get("SECRET_KEY", "mys3cr3tk3y")
+
 db = Database()
 
 @app.route('/')
 def index():
     data = db.read(None)
-
-    return render_template('index.html', data = data)
+    return render_template('index.html', data=data)
 
 @app.route('/add/')
 def add():
     return render_template('add.html')
 
-@app.route('/addphone', methods = ['POST', 'GET'])
+@app.route('/addphone', methods=['POST', 'GET'])
 def addphone():
-    if request.method == 'POST' and request.form['save']:
+    if request.method == 'POST' and request.form.get('save'):
         if db.insert(request.form):
             flash("A new phone number has been added")
         else:
-            flash("A new phone number can not be added")
-
+            flash("A new phone number cannot be added")
         return redirect(url_for('index'))
-    else:
-        return redirect(url_for('index'))
+    return redirect(url_for('index'))
 
 @app.route('/update/<int:id>/')
 def update(id):
-    data = db.read(id);
-
+    data = db.read(id)
     if len(data) == 0:
         return redirect(url_for('index'))
-    else:
-        session['update'] = id
-        return render_template('update.html', data = data)
+    session['update'] = id
+    return render_template('update.html', data=data)
 
-@app.route('/updatephone', methods = ['POST'])
+@app.route('/updatephone', methods=['POST'])
 def updatephone():
-    if request.method == 'POST' and request.form['update']:
-
+    if request.method == 'POST' and request.form.get('update'):
         if db.update(session['update'], request.form):
             flash('A phone number has been updated')
-
         else:
-            flash('A phone number can not be updated')
-
+            flash('A phone number cannot be updated')
         session.pop('update', None)
-
         return redirect(url_for('index'))
-    else:
-        return redirect(url_for('index'))
+    return redirect(url_for('index'))
 
 @app.route('/delete/<int:id>/')
 def delete(id):
-    data = db.read(id);
-
+    data = db.read(id)
     if len(data) == 0:
         return redirect(url_for('index'))
-    else:
-        session['delete'] = id
-        return render_template('delete.html', data = data)
+    session['delete'] = id
+    return render_template('delete.html', data=data)
 
-@app.route('/deletephone', methods = ['POST'])
+@app.route('/deletephone', methods=['POST'])
 def deletephone():
-    if request.method == 'POST' and request.form['delete']:
-
+    if request.method == 'POST' and request.form.get('delete'):
         if db.delete(session['delete']):
             flash('A phone number has been deleted')
-
         else:
-            flash('A phone number can not be deleted')
-
+            flash('A phone number cannot be deleted')
         session.pop('delete', None)
-
         return redirect(url_for('index'))
-    else:
-        return redirect(url_for('index'))
+    return redirect(url_for('index'))
 
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('error.html')
 
+# Only used in LOCAL development, not in Docker
 if __name__ == '__main__':
-    app.run(port=8181, host="0.0.0.0")
+    app.run(debug=True)
